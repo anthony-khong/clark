df0e99dfdeefb0be6bafcc2f1a9e21fcb58e087c
Anthony Khong <anthony.kusumo.khong@gmail.com>
2019-07-19 15:00:19 +0700

    Refactored rename to use Flambo

--- src/clark/core.clj
+++ src/clark/core.clj
@@ -1,5 +1,5 @@
 (ns clark.core
-  (:require [flambo.sql :as sql :refer [create-custom-schema group-by agg order-by window over]]
+  (:require [flambo.sql :as sql]
             [flambo.sql-functions :refer [col]]
             [flambo.api :as api]
             [clojure.reflect :refer [reflect]]
@@ -26,57 +26,48 @@
                              "inferSchema" "true"})
                    (csv "data.csv")))
 
-(.printSchema dataframe)
-
-(->> dataframe
-     .columns
-     (map println))
-
-
-(->> spark
-     #(.textFile % "data.csv"))
-
-(.textFile spark "data.csv")
 
 ;; Utility Functions
-(defn select [dataframe column-names]
-  (let [columns (map #(Column. %) column-names)]
-    (select-cols dataframe columns)))
 
-(defn select-cols [dataframe columns]
-  (->> columns
-       scala.collection.JavaConversions/asScalaBuffer
-       (.select dataframe)))
+(defn col-as [expr alias]
+  (.as (col expr) alias))
+
+(defn escape-keys [some-map]
+  (into {} (map (fn [[k v]] [(str "`" k "`") v]) some-map)))
 
 (defn rename [dataframe rename-map]
-  (let [columns (map
-                 (fn [[old-name new-name]]
-                   (.as (Column. old-name) new-name))
-                 rename-map)]
-    (select-cols dataframe columns)))
-
+  (let [columns (sql/columns dataframe)
+        renamed-exprs (->> rename-map
+                           (merge (zipmap columns columns))
+                           escape-keys
+                           (map #(apply col-as %)))]
+    (apply sql/select dataframe renamed-exprs)))
 
 (-> dataframe
-    (rename {"Clicks" "clicks"
+    (rename {"Keyword status" "keyword_status"
+             "Keyword" "keyword"
+             "Campaign" "campaign"
+             "Ad group" "ad_group"
+             "Currency code" "currency_code"
+             "Status" "status"
+             "Max. CPC" "max_cpc"
+             "Policy details" "policy_details"
+             "Final URL" "final_url"
+             "Mobile final URL" "mobile_final_url"
+             "Clicks" "clicks"
+             "Impr." "impressions"
              "CTR" "ctr"
-             "`Conv. rate`" "cr"})
-    .show)
-
-(-> dataframe
-    (.agg))
-
-(.agg dataframe
-      (scala.collection.JavaConversions/asScalaBuffer
-       [(functions/sum (Column. "Clicks"))]))
-
-(.agg dataframe
-      (functions/sum (Column. "Clicks"))
-      (scala.collection.JavaConversions/asScalaBuffer []))
+             "Avg. CPC" "avg_cpc"
+             "Cost" "cost"
+             "Conversions" "conversions"
+             "Cost / conv." "cost_per_conv"
+             "Conv. rate" "cr"})
+    sql/print-schema)
 
 ;; TODO: Abstract out asScalaBuffer
 ;; TODO: Abstract out columns
 ;; TODO: Abstract out aggregates
 ;; TODO: Have a describe function
 ;; TODO: Try cleaning the percentages
-(.show (.describe dataframe
-           (scala.collection.JavaConversions/asScalaBuffer ["Clicks"])))
+;; (.show (.describe dataframe
+;;            (scala.collection.JavaConversions/asScalaBuffer ["Clicks"])))

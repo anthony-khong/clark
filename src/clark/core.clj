2363cce810fffbd6327f9aacd5c7818b48d7d90f
Anthony Khong <anthony.kusumo.khong@gmail.com>
2019-07-20 14:23:26 +0700

    Some data munging

--- src/clark/core.clj
+++ src/clark/core.clj
@@ -1,17 +1,8 @@
 (ns clark.core
   (:require [flambo.sql :as sql]
-            [flambo.sql-functions :refer [col]]
-            [flambo.api :as api]
-            [clojure.reflect :refer [reflect]]
-            [clojure.pprint :refer [print-table]])
+            [flambo.sql-functions :refer [col]])
   (:import (org.apache.spark.sql SparkSession)
-           (org.apache.spark SparkContext)
-           (org.apache.spark.api.java JavaSparkContext)
-           (org.apache.spark.sql Column)
-           (org.apache.spark.sql functions RowFactory)
-           (org.apache.spark.sql.types DataTypes)
-           (org.apache.spark.sql.expressions Window)
-           (org.apache.spark SparkContext))
+           (org.apache.spark.sql functions))
   (:gen-class))
 
 (defonce spark (.. (SparkSession/builder)
@@ -19,16 +10,14 @@
                    (master "local[*]")
                    getOrCreate))
 
-(def dataframe (.. spark
-                   read
-                   (options {"header" "true"
-                             "date" "yyyy-MM-dd"
-                             "inferSchema" "true"})
-                   (csv "data.csv")))
-
+(def raw-dataframe (.. spark
+                       read
+                       (options {"header"      "true"
+                                 "date"        "yyyy-MM-dd"
+                                 "inferSchema" "true"})
+                       (csv "data.csv")))
 
 ;; Utility Functions
-
 (defn col-as [expr alias]
   (.as (col expr) alias))
 
@@ -43,31 +32,72 @@
                            (map #(apply col-as %)))]
     (apply sql/select dataframe renamed-exprs)))
 
-(-> dataframe
-    (rename {"Keyword status" "keyword_status"
-             "Keyword" "keyword"
-             "Campaign" "campaign"
-             "Ad group" "ad_group"
-             "Currency code" "currency_code"
-             "Status" "status"
-             "Max. CPC" "max_cpc"
-             "Policy details" "policy_details"
-             "Final URL" "final_url"
-             "Mobile final URL" "mobile_final_url"
-             "Clicks" "clicks"
-             "Impr." "impressions"
-             "CTR" "ctr"
-             "Avg. CPC" "avg_cpc"
-             "Cost" "cost"
-             "Conversions" "conversions"
-             "Cost / conv." "cost_per_conv"
-             "Conv. rate" "cr"})
-    sql/print-schema)
+(defn describe [dataframe expr & exprs]
+  (.describe dataframe (into-array java.lang.String (conj exprs expr))))
 
-;; TODO: Abstract out asScalaBuffer
-;; TODO: Abstract out columns
-;; TODO: Abstract out aggregates
-;; TODO: Have a describe function
-;; TODO: Try cleaning the percentages
-;; (.show (.describe dataframe
-;;            (scala.collection.JavaConversions/asScalaBuffer ["Clicks"])))
+(defn sample [dataframe with-replacement fraction]
+  (.sample dataframe with-replacement fraction))
+
+(defn limit [dataframe n-rows]
+  (.limit dataframe n-rows))
+
+;; Actual Script
+(def dataframe
+  (-> raw-dataframe
+      (rename {"Keyword status"   "keyword_status"
+               "Keyword"          "keyword"
+               "Campaign"         "campaign"
+               "Ad group"         "ad_group"
+               "Currency code"    "currency_code"
+               "Status"           "status"
+               "Max. CPC"         "max_cpc"
+               "Policy details"   "policy_details"
+               "Final URL"        "final_url"
+               "Mobile final URL" "mobile_final_url"
+               "Clicks"           "clicks"
+               "Impr."            "impressions"
+               "CTR"              "ctr"
+               "Avg. CPC"         "avg_cpc"
+               "Cost"             "cost"
+               "Conversions"      "conversions"
+               "Cost / conv."     "cost_per_conv"
+               "Conv. rate"       "cr"})
+      (.withColumn "cr" (-> (col "cr")
+                            (functions/regexp_replace "%" "")
+                            (.divide (functions/lit 100))
+                            (.cast "float")))
+      (.withColumn "clicks" (-> (col "clicks")
+                                (.cast "float")))))
+
+
+;; (-> dataframe
+;;     (limit 10000)
+;;     (.filter (.isNotNull (col "keyword")))
+;;     (.withColumn "kwcost"
+;;                  (.over (functions/sum "cost")
+;;                         (sql/partition-by (sql/window) "keyword")))
+;;     (.filter (.notEqual (col "kwcost") (col "cost")))
+;;     (sql/select "kwcost" "cost")
+;;     ;; (sql/group-by "keyword")
+;;     ;; (sql/agg (.as (functions/sum "cost") "freq"))
+;;     .show)
+
+;; (println (sql/print-schema dataframe) (sql/count dataframe))
+
+
+;; (-> dataframe
+;;     (sql/agg (functions/sum "clicks")
+;;              (functions/max "ctr"))
+;;     sql/show)
+
+
+;; (-> dataframe
+;;     (describe "clicks")
+;;     sql/show)
+
+
+;; (-> dataframe
+;;     (sql/select "cr")
+;;     sql/show)
+
+;; (sql/print-schema dataframe)
